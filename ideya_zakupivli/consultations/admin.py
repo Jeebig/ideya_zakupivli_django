b115a52eab62acc8f14bcd579dd82bb5d5e422f9
ya_Rei <petro_kt00@student.itstep.org>
2026-09-08 17:03:29 +0300

    feat: Add inline support for ConsultationAttachment in ConsultationRequest admin; enhance admin interface for better file management

--- ideya_zakupivli/consultations/admin.py
+++ ideya_zakupivli/consultations/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import ConsultationRequest, ConsultationAttachment
 
 
+class ConsultationAttachmentInline(admin.TabularInline):
+    model = ConsultationAttachment
+    extra = 0
+    readonly_fields = ('uploaded_at', 'file')
+
+
 @admin.register(ConsultationRequest)
 class ConsultationRequestAdmin(admin.ModelAdmin):
     list_display = ('request_number', 'name', 'audience', 'response_method', 'topic', 'contact', 'created_at', 'is_processed')
     list_filter = ('is_processed', 'created_at')
     search_fields = ('name', 'contact', 'topic', 'message')
     readonly_fields = ('created_at',)
+    inlines = (ConsultationAttachmentInline,)
 
 
 @admin.register(ConsultationAttachment)
